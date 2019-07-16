3f5d8c5e6c094180f55f442dcf7dfbbf5c6a35e1
Gillian Shanks <GillianShanks88@gmail.com>
2019-07-16 16:06:14 +0100

    number_to_short_month_name(1) tested

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -60,3 +60,34 @@ def number_to_full_month_name(month_number)
     "There isn't that number of months!"
   end
 end
+
+def number_to_short_month_name(month_number)
+  case month_number
+  when 1
+    "Jan"
+  when 2
+    "Feb"
+  when 3
+    "Mar"
+  when 4
+    "Apr"
+  when 5
+    "May"
+  when 6
+    "Jun"
+  when 7
+    "Jul"
+  when 8
+    "Aug"
+  when 9
+    "Sept"
+  when 10
+    "Oct"
+  when 11
+    "Nov"
+  when 12
+    "Dec"
+  else
+    "There isn't that number of months!"
+  end
+end
